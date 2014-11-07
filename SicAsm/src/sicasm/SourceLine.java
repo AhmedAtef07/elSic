daaa71114e193c5b2b811d198aaec7f6bab7310f
AhmedAtef07 <ahmedatef07@hotmail.com>
2014-11-07 02:49:00 +0200

    Setting 'SourceLine' class.
    Basic data needed in 'SourceLine' class.

--- SicAsm/src/sicasm/SourceLine.java
+++ SicAsm/src/sicasm/SourceLine.java
@@ -1,5 +1,32 @@
 package sicasm;
 
 public class SourceLine {
+    private String label, 
+            mnemonic, 
+            operand, 
+            comment;
 
+    public SourceLine(String label, String mnemonic, String operand, 
+                      String comment) {
+        this.label = label;
+        this.mnemonic = mnemonic;
+        this.operand = operand;
+        this.comment = comment;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getMnemonic() {
+        return mnemonic;
+    }
+
+    public String getOperand() {
+        return operand;
+    }
+
+    public String getComment() {
+        return comment;
+    }    
 }
