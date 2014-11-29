c56ddd1dbf5a26ebf6fb54b274ddb22d92e1378a
Ahmed Wael Said <ahmedws93@hotmail.com>
2014-11-29 14:32:58 +0200

    Adding GUI changing main to accept GUI and test.
    Note : Listfile button and ObjFile button still under construction ..
    still something wrong with representing code .

--- SicAsm/src/sicasm/SicAsm.java
+++ SicAsm/src/sicasm/SicAsm.java
@@ -5,14 +5,10 @@ import java.io.IOException;
 public class SicAsm {
     
     public static void main(String[] args) throws IOException {
-        try {
-            assemble("SRCFILE03");
-        } catch (Exception e) {
-            System.out.println("Something wrong happened!\n" + e.getMessage());
-        }
-    }
-    
-    static void assemble(String fileName) throws IOException {
-        new ObjectFile(fileName, true);
-    }
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new GUI();
+            }
+        });        
+    }    
 }
