cca23e38ccd7f66491bf90a8a8e50a0d27ea8743
Ahmed Wael Said <ahmedws93@hotmail.com>
2014-12-22 23:15:02 +0200

    Adding key listener.

--- SicAsm/src/sicasm/GUI.java
+++ SicAsm/src/sicasm/GUI.java
@@ -1,41 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sicasm;
 
 /**
  *
- * @author user
+ * @author HackerGhost
  */
 //import com.sun.javafx.css.Rule;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.Clock;
 import java.util.Scanner;
-import javax.swing.Box;
+import java.util.Spliterators;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 public class GUI extends JFrame {
-
-	//JButton LisFile = new JButton("List File");
-	//JButton ObjFile = new JButton("Object File");  
+        private String absolutePath ;
+	JButton LisFile = new JButton("List File");
+	JButton ObjFile = new JButton("Object File");  
 	JButton Run = new JButton("Run Assembler");
 	JButton Clear = new JButton("Clear");
 	JButton Save = new JButton("Save");
@@ -43,12 +48,13 @@ public class GUI extends JFrame {
         //private Rule columnView;
         //private Rule rowView;
 	JTextArea input = new JTextArea(30,70);
-	JScrollPane scroll ;//= new JScrollPane(input);//,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+	JScrollPane scroll ;
 	JFileChooser chooser = new JFileChooser();
 	JPanel p= new JPanel();
 	JPanel west = new JPanel();
 	JPanel south = new JPanel();
 	JLabel status = new JLabel();
+        JPanel components = new JPanel();
         JFrame frame = new JFrame();
 	
 		
@@ -56,42 +62,85 @@ public class GUI extends JFrame {
 	GUI()
 	{
 		super("Assember Application");
-		setSize(1200,600);
+                setSize(900,600);
+		//frame.setSize(900,600);
                 scroll = new JScrollPane(input);
                 scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		setResizable(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-                 setLayout(new BorderLayout());
+                p.setLayout(new BorderLayout());
                 setContentPane(new JLabel(new ImageIcon("Boston-City.jpg")));
                 //p.setLayout(new FlowLayout());
                 setLayout (new FlowLayout());
                 p.setSize(200,200);
+                components.setSize(200,200);
 		input.setEditable(true);
 		input.setLineWrap(true);
 		input.setWrapStyleWord(true);
                 input.setFont(new Font("monospaced", Font.PLAIN, 12));
-	/*	LisFile.addActionListener(new ActionListener() {
+                this.requestFocusInWindow();
+                input.addKeyListener(new KeyListener() {
+
+                    @Override
+                    public void keyTyped(KeyEvent e) {
+                        System.out.println("key pressed"); 
+                        input.requestFocus();
+                        status.setText("Pressed");
+                    }
+                    @Override
+                    public void keyPressed(KeyEvent e) {
+                        System.out.println("key pressed");
+                        input.requestFocus();
+                        status.setText("Pressed");
+                    } 
+                    @Override
+                    public void keyReleased(KeyEvent e) {
+                        System.out.println("key pressed");
+                        input.requestFocus();
+                        status.setText("Pressed");
+                    }
+                } 
+                );
+		LisFile.addActionListener(new ActionListener() {
 			 
             public void actionPerformed(ActionEvent e)
             {
-                
+                   Runtime rt=Runtime.getRuntime();
+
+        //String file="E:\\Studies\\ProgII\\Lab\\Notepad\\LISFILE.txt";
+
+                try {
+                    Process p=rt.exec("notepad "+ absolutePath); 
+                    // a space is required after notepad
+                } catch (IOException ex) {
+                   status.setText("Can't be opened ");
+                }
             }
         }); 
 		ObjFile.addActionListener(new ActionListener() {
 			 
             public void actionPerformed(ActionEvent e)
             {
-                
+                 Runtime rt=Runtime.getRuntime();
+
+        //String file="E:\\Studies\\ProgII\\Lab\\Notepad\\LISFILE.txt";
+
+                try {
+                    Process p=rt.exec("notepad "+ absolutePath); // a space is required after notepad
+                } catch (IOException ex) {
+                   status.setText("Can't be opened ");
+                }
             }
-        });*/   
+        });  
 		Run.addActionListener(new ActionListener() {
 			 
             public void actionPerformed(ActionEvent e)
             {
                 try {
-                    new ObjectFile(Saving(), true);
+                    new ObjectFile(absolutePath=Saving(), true);
+                    status.setText("Assembled in " + absolutePath);
                 } catch (Exception ex) {
-                    System.out.println("Something wrong happened!\n" + ex.getMessage());
+                   status.setText("Something went wrong");
                 }
             }
         });  
@@ -99,6 +148,15 @@ public class GUI extends JFrame {
 			 
             public void actionPerformed(ActionEvent e)
             {
+                int dialogResult = JOptionPane.showConfirmDialog (frame, "Would You Like to Save your Previous Code First?","Warning",JOptionPane.YES_NO_OPTION);
+                if(dialogResult == JOptionPane.YES_OPTION){
+                    absolutePath=Saving();
+                if(absolutePath != null) status.setText("Cleared & saved in "  + absolutePath);
+                else status.setText("Cleared but not saved");
+                }
+                else {
+                    status.setText("Cleared without saving ");
+                }
                input.setText(null);
             }
         });  
@@ -106,7 +164,7 @@ public class GUI extends JFrame {
 			 
             public void actionPerformed(ActionEvent e)
             {
-               Saving();
+               absolutePath=Saving();
             }
         });  
 		Load.addActionListener(new ActionListener() {
@@ -117,25 +175,36 @@ public class GUI extends JFrame {
             }
         });  
 		//p.add(input);
-		p.add(scroll);
-               // p.add( Box.createVerticalStrut(400) );
-		west.setLayout(new GridLayout(4,1));
+                //p.add( Box.createVerticalStrut(400) );
+		//p.add(scroll);
+		west.setLayout(new GridLayout(6,1));
 		west.add(Clear);
-		west.add(Save);
 		west.add(Load);
-		//west.add(LisFile);
-		//west.add(ObjFile);
+		west.add(Save);
 		west.add(Run);
-		status.setText("Waiting");
+		west.add(LisFile);
+		west.add(ObjFile);
 		south.add(status);
-		add(west);
-		add(south,BorderLayout.SOUTH);
-		add(p,BorderLayout.CENTER);
+		status.setText("Waiting");
+                components.setBackground(new Color(0,0,0,50));
+                components.setLayout(new BorderLayout());
+		components.add(west,BorderLayout.WEST);
+                components.add(south,BorderLayout.SOUTH);
+                components.add(scroll,BorderLayout.CENTER);
+		//add(south,BorderLayout.SOUTH);
+                p.add(components);
+                add(p);
 		setVisible(true);
                 //frame.setVisible(true);
-		
 	}
-	public void Loading(){
+	
+        
+        
+        
+        
+        
+        
+        public void Loading(){
 		int chooserValue = chooser.showOpenDialog(this);
 		if(chooserValue == chooser.APPROVE_OPTION){
 			try{
@@ -149,7 +218,6 @@ public class GUI extends JFrame {
 			}
 			catch (FileNotFoundException ex)
 			{
-				//JOptionPane.showMessageDialog(this,"File");
                             status.setText("Couldn't be loaded");
 			}
 		}
@@ -159,6 +227,7 @@ public class GUI extends JFrame {
 		if(chooserValue == chooser.APPROVE_OPTION){
 			try{
                             File selectedfile =chooser.getSelectedFile();
+                            if(selectedfile.getAbsolutePath() != null){
 			PrintWriter writer = new PrintWriter(chooser.getSelectedFile());
 			String s = input.getText();
 			String[] lines = s.split("\\n");
@@ -167,6 +236,8 @@ public class GUI extends JFrame {
 			writer.close();
                             status.setText("File saved in " + selectedfile.getAbsolutePath());
                         return selectedfile.getAbsolutePath();
+                            }
+                            else status.setText("Nothing was saved");
 			}
 			catch(FileNotFoundException ex){
                             status.setText("Error saving File");
@@ -175,4 +246,23 @@ public class GUI extends JFrame {
 		}
 		return null ;
 	}
+        
+        
+        public String checker(){
+            StringBuilder str = new StringBuilder();
+            String temp ;
+            temp = input.getText();
+            for( int i=temp.length()-1;  i>=0 &&  (temp.charAt(i)) != 32 ; i-- )
+            {
+                str.append(temp.charAt(i));
+            }
+            str.reverse();
+            System.out.println(str.toString());
+            
+            if(Constants.OpTable.containsKey(str.toString())){
+                
+            }
+            
+            return str.toString();
+        }
 }
