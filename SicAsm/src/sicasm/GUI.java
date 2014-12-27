0c48f13a36314f4c8cea665e0cd0a611cb6c9076
Ahmed Wael Said <ahmedws93@hotmail.com>
2014-12-27 13:59:01 +0200

    GUI commit

--- SicAsm/src/sicasm/GUI.java
+++ SicAsm/src/sicasm/GUI.java
@@ -1,3 +1,4 @@
+
 package sicasm;
 
 /**
@@ -26,6 +27,7 @@ import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 
 import javax.swing.JButton;
+import javax.swing.text.DefaultHighlighter;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
@@ -36,20 +38,27 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.text.Highlighter;
 
 public class GUI extends JFrame {
+        private String RunningAbsolutePath;
         private String absolutePath ;
+        private boolean helperBoolean;
 	JButton LisFile = new JButton("List File");
+        Highlighter.HighlightPainter mySelector = new MyPainter(Color.BLUE);
 	JButton ObjFile = new JButton("Object File");  
 	JButton Run = new JButton("Run Assembler");
 	JButton Clear = new JButton("Clear");
 	JButton Save = new JButton("Save");
 	JButton Load= new JButton("Load");
-        //private Rule columnView;
-        //private Rule rowView;
+        JButton SaveAs = new JButton("Save As");
+        JButton Help = new JButton("Help");
+        String helping  = "Ctrl S -> Save\nCtrl O ->Open\nCtrl Shift S -> Save as\n"
+                + "Ctrl L -> Listfile\n Ctrl J -> ObjFile\nCtrl R -> Run";
 	JTextArea input = new JTextArea(30,70);
 	JScrollPane scroll ;
 	JFileChooser chooser = new JFileChooser();
+        JTextArea helper = new JTextArea(helping);
 	JPanel p= new JPanel();
 	JPanel west = new JPanel();
 	JPanel south = new JPanel();
@@ -68,12 +77,15 @@ public class GUI extends JFrame {
                 scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		setResizable(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-                p.setLayout(new BorderLayout());
+                // la3abt hena 
+                
+                //p.setLayout(new FlowLayout());
+               // p.setLayout(new BorderLayout());
                 setContentPane(new JLabel(new ImageIcon("Boston-City.jpg")));
                 //p.setLayout(new FlowLayout());
                 setLayout (new FlowLayout());
-                p.setSize(200,200);
-                components.setSize(200,200);
+                //p.setSize(200,200);
+                //components.setSize(200,200);
 		input.setEditable(true);
 		input.setLineWrap(true);
 		input.setWrapStyleWord(true);
@@ -83,21 +95,51 @@ public class GUI extends JFrame {
 
                     @Override
                     public void keyTyped(KeyEvent e) {
-                        System.out.println("key pressed"); 
-                        input.requestFocus();
-                        status.setText("Pressed");
+                        
                     }
                     @Override
                     public void keyPressed(KeyEvent e) {
-                        System.out.println("key pressed");
+                        
                         input.requestFocus();
-                        status.setText("Pressed");
+                        // lesa msh sha3'ala garab el control shift
+                         if((e.getKeyCode() == KeyEvent.VK_S)  &&  
+                                 e.isControlDown() && e.isShiftDown() ){
+                             Saving();
+                         }
+                         else if ((e.getKeyCode() == KeyEvent.VK_S) && 
+                                 ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)){
+                             Save();
+                         }
+                         else if((e.getKeyCode() == KeyEvent.VK_O) && 
+                                 ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)){
+                             Loading();
+                         }
+                         else if((e.getKeyCode() == KeyEvent.VK_L) && 
+                                 ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)){
+                             lis();
+                         }
+                         else if((e.getKeyCode() == KeyEvent.VK_J) && 
+                                 ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)){
+                             obj();
+                         }
+                         else if((e.getKeyCode() == KeyEvent.VK_H) && 
+                                 ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)){
+                             help();
+                         }
+                         else if((e.getKeyCode() == KeyEvent.VK_C) && 
+                                 ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)){
+                             clearing();
+                         }
+                         else if((e.getKeyCode() == KeyEvent.VK_R) && 
+                                 ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)){
+                             Running();
+                         }
+                         else status.setText("Think before you code .. Coders don't need luck ");
+                       
                     } 
                     @Override
                     public void keyReleased(KeyEvent e) {
-                        System.out.println("key pressed");
-                        input.requestFocus();
-                        status.setText("Pressed");
+                       
                     }
                 } 
                 );
@@ -105,59 +147,28 @@ public class GUI extends JFrame {
 			 
             public void actionPerformed(ActionEvent e)
             {
-                   Runtime rt=Runtime.getRuntime();
-
-        //String file="E:\\Studies\\ProgII\\Lab\\Notepad\\LISFILE.txt";
-
-                try {
-                    Process p=rt.exec("notepad "+ absolutePath); 
-                    // a space is required after notepad
-                } catch (IOException ex) {
-                   status.setText("Can't be opened ");
-                }
+                  list();
             }
         }); 
 		ObjFile.addActionListener(new ActionListener() {
 			 
             public void actionPerformed(ActionEvent e)
             {
-                 Runtime rt=Runtime.getRuntime();
-
-        //String file="E:\\Studies\\ProgII\\Lab\\Notepad\\LISFILE.txt";
-
-                try {
-                    Process p=rt.exec("notepad "+ absolutePath); // a space is required after notepad
-                } catch (IOException ex) {
-                   status.setText("Can't be opened ");
-                }
+                obj();
             }
         });  
 		Run.addActionListener(new ActionListener() {
 			 
             public void actionPerformed(ActionEvent e)
             {
-                try {
-                    new ObjectFile(absolutePath=Saving(), true);
-                    status.setText("Assembled in " + absolutePath);
-                } catch (Exception ex) {
-                   status.setText("Something went wrong");
-                }
+                Running();
             }
         });  
 		Clear.addActionListener(new ActionListener() {
 			 
             public void actionPerformed(ActionEvent e)
             {
-                int dialogResult = JOptionPane.showConfirmDialog (frame, "Would You Like to Save your Previous Code First?","Warning",JOptionPane.YES_NO_OPTION);
-                if(dialogResult == JOptionPane.YES_OPTION){
-                    absolutePath=Saving();
-                if(absolutePath != null) status.setText("Cleared & saved in "  + absolutePath);
-                else status.setText("Cleared but not saved");
-                }
-                else {
-                    status.setText("Cleared without saving ");
-                }
-               input.setText(null);
+                clearing();
             }
         });  
 		Save.addActionListener(new ActionListener() {
@@ -166,45 +177,99 @@ public class GUI extends JFrame {
             {
                absolutePath=Saving();
             }
-        });  
+        });
+                SaveAs.addActionListener(new ActionListener() {
+
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        Saving();
+                    }
+                });
 		Load.addActionListener(new ActionListener() {
 			 
             public void actionPerformed(ActionEvent e)
             {
                Loading();
             }
-        });  
+        }); 
+                Help.addActionListener(new ActionListener() {
+
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        help();
+                    }
+                });
 		//p.add(input);
+                this.helperBoolean = true ;
                 //p.add( Box.createVerticalStrut(400) );
 		//p.add(scroll);
-		west.setLayout(new GridLayout(6,1));
+		west.setLayout(new GridLayout(8,1));
 		west.add(Clear);
 		west.add(Load);
 		west.add(Save);
+                west.add(SaveAs);
 		west.add(Run);
 		west.add(LisFile);
 		west.add(ObjFile);
+                west.add(Help);
 		south.add(status);
 		status.setText("Waiting");
                 components.setBackground(new Color(0,0,0,50));
+                //components.set
                 components.setLayout(new BorderLayout());
 		components.add(west,BorderLayout.WEST);
                 components.add(south,BorderLayout.SOUTH);
                 components.add(scroll,BorderLayout.CENTER);
 		//add(south,BorderLayout.SOUTH);
-                p.add(components);
-                add(p);
+              //  p.add(components);
+               // add(p);
+                add(helper);
+                add(components);
 		setVisible(true);
                 //frame.setVisible(true);
 	}
 	
         
         
+        private String Save(){
+            if(absolutePath == null)
+                return Saving();
+            else
+            {
+                try{
+                            File selectedfile =new File(absolutePath);
+                            if(selectedfile.getAbsolutePath() != null){
+			PrintWriter writer = new PrintWriter(chooser.getSelectedFile());
+			String s = input.getText();
+			String[] lines = s.split("\\n");
+			for(int i=0;i<lines.length;i++)
+			writer.println(lines[i]);
+			writer.close();
+                            status.setText("File saved in " + selectedfile.getAbsolutePath());
+                        return selectedfile.getAbsolutePath();
+                            }
+                            else status.setText("Nothing was saved");
+			}
+			catch(FileNotFoundException ex){
+                            status.setText("Error saving File");
+			}
+                return null ;
+            }
+        }
         
         
-        
-        
-        public void Loading(){
+        private void help(){
+            if(helperBoolean){
+                this.helperBoolean = false;
+                helper.setVisible(helperBoolean);
+            }
+            else{
+                this.helperBoolean= true;
+                helper.setVisible(helperBoolean);
+            }
+            
+        }
+        private void Loading(){
 		int chooserValue = chooser.showOpenDialog(this);
 		if(chooserValue == chooser.APPROVE_OPTION){
 			try{
@@ -222,12 +287,24 @@ public class GUI extends JFrame {
 			}
 		}
 	}
-	public String Saving(){
+        
+        private void Running(){
+            try {
+                   
+                    new ObjectFile(absolutePath=Saving(), true);
+                    RunningAbsolutePath = absolutePath;
+                    status.setText("Assembled in " + absolutePath);
+                } catch (Exception ex) {
+                   status.setText("Something went wrong");
+                }
+        }
+	private String Saving(){
 		int chooserValue = chooser.showSaveDialog(this);
 		if(chooserValue == chooser.APPROVE_OPTION){
 			try{
                             File selectedfile =chooser.getSelectedFile();
                             if(selectedfile.getAbsolutePath() != null){
+                        absolutePath = selectedfile.getAbsolutePath();
 			PrintWriter writer = new PrintWriter(chooser.getSelectedFile());
 			String s = input.getText();
 			String[] lines = s.split("\\n");
@@ -247,6 +324,51 @@ public class GUI extends JFrame {
 		return null ;
 	}
         
+        private void lis(){
+            if(RunningAbsolutePath != null){
+             Runtime rt=Runtime.getRuntime();
+
+        //String file="E:\\Studies\\ProgII\\Lab\\Notepad\\LISFILE.txt";
+
+                try {
+                    Process p=rt.exec("notepad "+ absolutePath); 
+                    // a space is required after notepad
+                } catch (IOException ex) {
+                   status.setText("Can't be opened ");
+                }
+            }
+            else  status.setText("Nothing to be opened");
+        }
+         
+        private void clearing(){
+            int dialogResult = JOptionPane.showConfirmDialog (frame, "Would You "
+                    + "Like to Save your Previous Code First?",
+                         "Warning",JOptionPane.YES_NO_OPTION);
+                if(dialogResult == JOptionPane.YES_OPTION){
+                    absolutePath=Saving();
+                if(absolutePath != null) status.setText("Cleared & saved in " 
+                        + absolutePath);
+                else status.setText("Cleared but not saved");
+                input.setText(null);
+                }
+                else {
+                    status.setText(" Command aboarded ");
+                }
+        }
+        
+        private void obj(){
+            if(RunningAbsolutePath != null){
+             Runtime rt=Runtime.getRuntime();
+                try {
+                    Process p=rt.exec("notepad "+ absolutePath); 
+                    status.setText("Opened");
+                } catch (IOException ex) {
+                   status.setText("Can't be opened");
+                }
+            }
+            else status.setText("Nothing Has been runned");
+        }
+        
         
         public String checker(){
             StringBuilder str = new StringBuilder();
@@ -264,5 +386,10 @@ public class GUI extends JFrame {
             }
             
             return str.toString();
+        }
+        private class MyPainter extends DefaultHighlighter.DefaultHighlightPainter{
+            public MyPainter(Color SelectorColor){ 
+            super(SelectorColor);
+            }
         }
 }
